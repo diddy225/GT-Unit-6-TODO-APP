0e1d3577358ae6be106a8cd838269e51254ef4d2
Matthew C <matthew.j.carpenter1@gmail.com>
2018-10-02 18:02:11 -0400

    updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 
 // mongoose.connect('mongodb://localhost:27017/todoapp', { useNewUrlParser: true });
-mongoose.connect('mongodb://mcarpenter:test12@ds119523.mlab.com:19523/heroku_w09m497r/todolists', { useNewUrlParser: true })
+mongoose.connect('mongodb://mcarpenter:test12@ds119523.mlab.com:19523/heroku_w09m497r/', { useNewUrlParser: true })
 
 require('./routes/api-routes.js')(app);
 
